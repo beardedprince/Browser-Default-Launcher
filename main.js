80a67873c2c8e29196eb1e69d309b3f12d8414ca
beardedprince <alaketuolamilekan02@gmail.com>
2019-07-19 14:17:49 +0100

    Function to retrieve data from localstorage

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//Toggle settings button
 function openSettings() {
    document.getElementById("settings").classList.toggle("settings-opened");
    
@@ -20,13 +20,11 @@ function saveName() {
 
 function changeName() {
    userName = document.getElementById("name-input").value;
+   
    saveName();
    getGreeting();
 
 }
-
-
-
 
 document.getElementById("name-form").addEventListener('submit', function(e) {
    e.preventDefault()
@@ -62,67 +60,97 @@ var timing = document.getElementById("timing").innerHTML= time;
         greet = 'Good evening';
 
     document.getElementById('time-of-day').innerHTML =
-        '<b>' + greet + '</b> ';
+        '<b>' + greet + '</b>';
 
-//todo app settings
 
-$("document").ready(function(){
-localStorage.getItem("tasks");
-		$('.add').click(function(){
-			var acceptInput = $('.input').val();
-			localStorage.setItem("tasks", acceptInput);
-			
-			if (acceptInput == ''){
-				alert('you need to input something');
-			} else {
-			$('ul').append('<li> <i class="fa fa-edit"></i> ' + acceptInput + '</li>' )
-			localStorage.getItem("tasks");
-$('.input').val('');
+//todo app settings using jQuery
+
+var nextTask = [];
+
+		$("document").ready(function(){
+
+				$('.add').click(function(){
+					var acceptInput = $('.input').val();
+					
+						nextTask.push(acceptInput);
+						localStorage.setItem("tasks", JSON.stringify(nextTask));
+					
+					
+					if (acceptInput == '') {
+						alert('you need to input something');
+					} 
+					else {
+						$('ul').append('<li> <i class="fa fa-edit"></i> ' + acceptInput + '<i class="fa fa-trash" aria-hidden="true"></i>' + '' + '</li>');
+						$('.input').val('');
+
+					}
+				});
+					$(document).on('dblclick','li', function(){
+		      		$(this).toggleClass('strike').fadeOut('1000'); 
+
+		      });
+
+
+		});
 
 		
 
+// Retieve data from localStorage and parse the data to normal string
 
-			}});
+$(function(){
 
+	for (var i = 0; i < localStorage.length; i++) {
+		var a = localStorage.key(i);
+		var b = localStorage.getItem(a);
+		var c = '';
+		c += '<li>'  + JSON.parse(b) +  '</li>';
+	
+		$('ul').append(c); // Change output from localStorage to list tag
 
+		
 
-			$(document).on('dblclick','li', function(){
-        $(this).toggleClass('strike').fadeOut('1000');    
-      });
-
-			
-
-   });
-
-$( function() {
-    $( "ul.droptrue" ).sortable({
-      connectWith: "ul"
-    });
- 
-//     $('.droppable').droppable({ 
-
-//   // default options
-//   accept: '*',
-//   activeClass: '',
-//   disabled: false,
-//   hoverClass: '',
-//   initialized: false,
-//   scope: 'default',
-//   receiveHandler: null
-  
-// });
- 
-    $( "#sortable1" ).disableSelection();
-  } );
-
+		
+	}
 
 	
-function saveTask(){
-	var a = document.getElementById("input1").value;
-	localStorage.setItem("tasks", a);
-	
-}
+});
 
-function getTask(){
-	localStorage.getItem("tasks");
-}
+var array = ['slide 1', 'slide 2', 'slide 9', 'i don tire', 2019];
+
+
+
+    array.forEach(function(item){
+	
+    	$('ol').append('<li>' + item + '</li>');
+    	
+    })
+
+
+
+
+
+// Allow tasks to be sorted
+	$( function() {
+	    $( "ul.droptrue" ).sortable({
+	      connectWith: "ul"
+	    });
+	 
+	    $('.droppable').droppable({ 
+
+	  // default options
+	  accept: '*',
+	  activeClass: '',
+	  disabled: false,
+	  hoverClass: '',
+	  initialized: false,
+	  scope: 'default',
+	  receiveHandler: null
+	  
+	});
+	 
+	    $( "#sortable1" ).disableSelection();
+	  } );
+
+
+
+
